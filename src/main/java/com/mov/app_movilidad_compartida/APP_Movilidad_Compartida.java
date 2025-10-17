e93f51e8940c5cbfb943b7cd0fbb953ff43deb8e
Camila <camila.munoz@usil.pe>
2025-10-17 18:30:49 -0500

    Versión inicial del proyecto de movilidad compartida

---
+++ src/main/java/com/mov/app_movilidad_compartida/APP_Movilidad_Compartida.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mov.app_movilidad_compartida;
+
+/**
+ *
+ * @author camila
+ */
+public class APP_Movilidad_Compartida {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
