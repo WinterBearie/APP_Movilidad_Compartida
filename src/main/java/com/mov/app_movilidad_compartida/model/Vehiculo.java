0c3f369d51ac35a53d33cbe3e1abffe5d6b33116
USER <USER@DESKTOP-5HDDUSC>
2025-10-23 06:01:03 -0500

    Agregue la clase Vehiculo con atributos y validaciones

---
+++ src/main/java/com/mov/app_movilidad_compartida/model/Vehiculo.java
@@ -0,0 +1,40 @@
+package com.mov.app_movilidad_compartida.model;
+
+public class Vehiculo {
+    private String modelo;
+    private String placa;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String modelo, String placa) {
+        this.modelo = modelo;
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    
+    public boolean validarPlaca (){
+        return placa != null && placa.matches("[A-Z]{3}-\\d{3}");
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "modelo=" + modelo + ", placa=" + placa + '}';
+    }
+    
+}
